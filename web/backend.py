f05bae4a945fdac65c6d105c0d324cb0de95a3d5
yrzgithub <seenusanjay20102002@gmail.com>
2024-03-08 08:49:11 +0530

    face

--- web/backend.py
+++ web/backend.py
@@ -1,6 +1,97 @@
 from flask import *
 
 
+
+class Terrorist:
+
+    name = ""
+    img = None
+    age = 20
+    gender = ""
+    country = ""
+    group = ""
+    religion = ""
+    position = ""
+    illness = ""
+
+    def __init__(self,name,img):
+        self.name = name
+        self.img = img 
+    
+
+    def getName(self):
+        return self.name 
+    
+
+    def getImg(self):
+        return self.img 
+    
+
+    def getAge(self):
+        return self.age 
+    
+
+    def getGender(self):
+        return self.gender 
+    
+
+    def getCountry(self):
+        return self.country 
+    
+
+    def getGroup(self):
+        return self.group
+    
+
+    def getReligion(self):
+        return self.religion 
+    
+
+    def getPosition(self):
+        return self.position 
+    
+
+    def getIllness(self):
+        return self.illness
+    
+
+    def setName(self,name):
+        self.name = name 
+    
+
+    def setImg(self,img):
+        self.img = img
+    
+
+    def setAge(self,age):
+        self.age = age 
+    
+
+    def setGender(self,gender):
+        self.gender = gender
+    
+
+    def setCountry(self,country):
+        self.country = country
+    
+
+    def setGroup(self,group):
+        self.group = group 
+    
+
+    def setReligion(self,religion):
+        self.religion = religion
+    
+
+    def setPosition(self,position):
+        self.position = position
+    
+
+    def setIllness(self,illness):
+        self.illness = illness
+        
+
+
 app = Flask(__name__)
 
 
@@ -16,6 +107,16 @@ def detect():
     password = form["password"].strip()
     print(username,password)
     return render_template("detect.html")
+
+
+@app.route("/object/{weapon}")
+def obj(weapon):
+    return render_template("obj.html")
+
+
+@app.route("/face/{face}")
+def obj(face):
+    return render_template("face.html")
 
 
 app.run(debug=True)
