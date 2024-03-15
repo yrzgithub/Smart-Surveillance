b6bf5be53e0b30fce52fb364f829076fd32c76bf
yrzgithub <seenusanjay20102002@gmail.com>
2024-03-15 12:38:58 +0530

    Web Development Completed

--- web/static/js/home.js
+++ web/static/js/home.js
@@ -1,4 +1,25 @@
-function submit(element)
+function login()
 {
-    element.preventDefault();
+    var username = document.getElementById("username").value.trim();
+    var password = document.getElementById("password").value.trim();
+
+    if(username=="")
+    {
+        alert("Username cannot be empty.");
+        return false;
+    }
+
+    if(password=="")
+    {
+        alert("password cannot be empty.");
+        return false;
+    }
+
+    var spinner = document.getElementById("spinner");
+    var element = document.getElementById("submit");
+
+    spinner.className = "fa fa-spinner fa-spin spinner";
+
+    element.style.opacity = ".5";
+    element.disabled = true;
 }
